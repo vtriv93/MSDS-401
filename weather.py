39c96cf0823b957b9774a315e3755f344ed0b0b3
vtriv93 <63824725+vtriv93@users.noreply.github.com>
2020-04-16 21:25:56 -0400

    Delete weather.py

--- weather.py
+++
@@ -1,6 +0,0 @@
-import requests
-r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=8d2351c39c1fbec244c8a59227424628&units=imperial', auth=('user', 'pass'))
-print(r.status_code)
-print(r.json())
-j=r.json()
-print(j['main'])
